feb8f1c9187a8f1b4109a5d7d7a2136e60fa00c5
ADITYA JHA <amazingaditya999@gmail.com>
2026-03-27 02:17:14 +0530

    Remove dotenv loading from app.py
    Removed dotenv import and load function.

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from dotenv import load_dotenv
-load_dotenv()
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
